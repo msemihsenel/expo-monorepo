213fe6c8748699cffeff8ea53f6c6fdc3563e0dc
msemihsenel <semih.senel@logicalbond.com>
2021-02-25 20:06:44 +0300

    ADD common CustomInput

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,9 +1,11 @@
 import CustomCircleButton from './components/CustomCircleButton';
 import ComponentSecond from './components/ComponentSecond';
 import Component from './components/Component';
+import CustomInput from './components/CustomInput';
 
 export {
     CustomCircleButton,
     ComponentSecond,
-    Component
+    Component,
+    CustomInput
 };
